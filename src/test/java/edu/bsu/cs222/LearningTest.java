d6c5ec8dcfb565ed19bb342b1bdc844db7982fba
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-02-06 13:48:49 -0500

    Production code classes added. Test Text file and learning test created but not finished.

---
+++ src/test/java/edu/bsu/cs222/LearningTest.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs222;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LearningTest {
+    List<String> data = Files.readAllLines(Paths.get("Elizabeth_IIExampleFile.txt"));
+
+    public LearningTest() throws IOException {
+    }
+
+    boolean
+}
