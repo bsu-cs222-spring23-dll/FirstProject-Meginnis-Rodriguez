f723ba2f72e1f4083748bd861993eaeaf72206d9
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-02-06 17:04:32 -0500

    Learning Test completed

--- src/test/java/edu/bsu/cs222/LearningTest.java
+++ src/test/java/edu/bsu/cs222/LearningTest.java
@@ -1,14 +1,28 @@
 package edu.bsu.cs222;
-import java.io.IOException;
-import java.nio.file.Files;
+
+import java.io.*;
 import java.nio.file.Paths;
-import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class LearningTest {
-    List<String> data = Files.readAllLines(Paths.get("Elizabeth_IIExampleFile.txt"));
+    //adapted from https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+    @Test
+    public void testingFileForAuthorName() throws Exception {
+        File file = new File("src/main/java/edu/bsu/cs222/Elizabeth_IIExampleFile");
+        FileReader fileReader = new FileReader(file);
 
-    public LearningTest() throws IOException {
+        String fileInString = "";
+        int character;
+
+        while((character = fileReader.read()) != -1) {
+            fileInString = fileInString + (char)character;
+        }
+
+        System.out.println(fileInString);
+
+        boolean result = fileInString.contains("Neveselbert");
+        Assertions.assertTrue(result);
     }
-
-    boolean
 }
