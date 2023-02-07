828d46908c692afe4e32d415300ac26870fe746a
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-02-07 13:45:17 -0500

    Tutorial completed. New classes and tests added.

--- src/main/java/edu/bsu/cs222/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/RevisionParser.java
@@ -1,4 +1,13 @@
 package edu.bsu.cs222;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
+import java.io.IOException;
+import java.io.InputStream;
 public class RevisionParser {
+    public String parse(InputStream testDataStream) throws IOException {
+       JSONArray result = (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
+       return result.get(0).toString();
+    }
 }
---
+++ src/test/java/edu/bsu/cs222/RevisionParserTest.java
@@ -0,0 +1,18 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RevisionParserTest {
+
+    @Test
+    public void testParse() throws IOException {
+        RevisionParser parser = new RevisionParser();
+        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        String timestamp = parser.parse(testDataStream);
+        Assertions.assertEquals("2023-01-30T20:20:13Z", timestamp);
+    }
+}
