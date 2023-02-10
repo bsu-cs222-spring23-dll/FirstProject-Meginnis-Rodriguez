8439993272f49c06f8f8ced2bea9a1091d516172
Jrod1621 <julianrodriguez2121@gmail.com>
2023-02-10 11:42:52 -0500

    Revision formatter, creates list of users and timestamp of user edit to then be returned.

--- src/main/java/edu/bsu/cs222/RevisionFormatter.java
+++ src/main/java/edu/bsu/cs222/RevisionFormatter.java
@@ -1,4 +1,12 @@
 package edu.bsu.cs222;
 
-public class RevisionFormatter {
+import java.util.ArrayList;
+
+public class RevisionFormatter extends RevisionParser{
+    public ArrayList<String> revisionFormat (ArrayList<String> parsedUserList, ArrayList<String> parsedTimeStampList){
+        ArrayList<String> revisionList = new ArrayList<>();
+        for (int object = 0; object < parsedUserList.size(); object++){
+            revisionList.add(parsedTimeStampList.get(object) + " " + parsedUserList.get(object)+ "\n");
+        }return revisionList;
+    }
 }
