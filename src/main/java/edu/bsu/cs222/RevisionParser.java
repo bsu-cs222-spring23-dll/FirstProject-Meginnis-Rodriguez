46924dba3dd2eb024425b282a1357d553628b98b
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-02-09 23:33:42 -0500

    Added parseAccount method

--- src/main/java/edu/bsu/cs222/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/RevisionParser.java
@@ -5,11 +5,24 @@ import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
 public class RevisionParser {
     public Revision parse(InputStream testDataStream) throws IOException {
        JSONArray result = (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
        String timeStamp = result.get(0).toString();
        return new Revision(timeStamp);
     }
+
+    public ArrayList<String> parseAccount(InputStream wikipediaData) throws IOException {
+        JSONArray result = (JSONArray) JsonPath.read(wikipediaData, "$..user");
+        ArrayList<String> parsedAccounts = new ArrayList<>();
+        for (int i = 0; i < result.toArray().length; i++)
+        {
+            parsedAccounts.add(result.get(i).toString());
+        }
+        return parsedAccounts;
+    }
+
 
 }
