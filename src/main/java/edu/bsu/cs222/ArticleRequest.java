964c012cf7726ebfa9a87e5a0feb433608d21a7d
Jrod1621 <julianrodriguez2121@gmail.com>
2023-02-09 23:36:55 -0500

    Article Request, returns string if input is empty.

--- src/main/java/edu/bsu/cs222/ArticleRequest.java
+++ src/main/java/edu/bsu/cs222/ArticleRequest.java
@@ -1,4 +1,11 @@
 package edu.bsu.cs222;
 
 public class ArticleRequest {
+    public String articleSearch(String articleSearchRequest) {
+        if (articleSearchRequest.trim().equals("")) {
+            System.out.println("Search for article contained no input");
+            System.exit(0);
+        }
+        return articleSearchRequest;
+    }
 }
