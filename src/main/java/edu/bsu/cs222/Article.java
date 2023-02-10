3abf92139315543d39e146e7669f139aff43b9fd
Jrod1621 <julianrodriguez2121@gmail.com>
2023-02-10 00:14:36 -0500

    Article update, creates URL to be referenced and encodes. Changed limit to 27 on link.

--- src/main/java/edu/bsu/cs222/Article.java
+++ src/main/java/edu/bsu/cs222/Article.java
@@ -1,4 +1,11 @@
 package edu.bsu.cs222;
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
 public class Article {
+    public String generateURL(String articleName){
+        articleName = URLEncoder.encode(articleName, Charset.defaultCharset());
+        return String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=%s&rvprop=timestamp|user&rvlimit=27&redirects");
+    }
 }
