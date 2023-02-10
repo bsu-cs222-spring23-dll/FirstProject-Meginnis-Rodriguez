3d6537bc9b8bfa0cb4c9b012f925ed093502c3d1
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-02-10 12:31:37 -0500

    created ArticleRequestTest

--- src/main/java/edu/bsu/cs222/ArticleRequest.java
+++ src/main/java/edu/bsu/cs222/ArticleRequest.java
@@ -1,7 +1,7 @@
 package edu.bsu.cs222;
 
 public class ArticleRequest {
-    public String articleSearch(String articleSearchRequest) {
+    public static String articleSearch(String articleSearchRequest) {
         if (articleSearchRequest.trim().equals("")) {
             System.out.println("Search for article contained no input");
             System.exit(0);
