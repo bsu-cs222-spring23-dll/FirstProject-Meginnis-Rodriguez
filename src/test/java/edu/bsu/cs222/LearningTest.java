89107f0b18024a27faa4c413012a92ba6ac4b9a2
bgmeginnis <112116134+bgmeginnis@users.noreply.github.com>
2023-02-13 10:41:40 -0500

    All warnings fixed now

--- src/test/java/edu/bsu/cs222/LearningTest.java
+++ src/test/java/edu/bsu/cs222/LearningTest.java
@@ -8,20 +8,26 @@ import org.junit.jupiter.api.Test;
 public class LearningTest {
     //adapted from https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
     @Test
-    public void testingFileForAuthorName() throws Exception {
+    public void testingFileForAuthorName() {
         File file = new File("src/main/java/edu/bsu/cs222/Elizabeth_IIExampleFile");
-        FileReader fileReader = new FileReader(file);
 
-        StringBuilder fileInString = new StringBuilder();
-        int character;
+        try (FileReader fileReader = new FileReader(file)) {
 
-        while((character = fileReader.read()) != -1) {
-            fileInString.append((char) character);
+            StringBuilder fileInString = new StringBuilder();
+            int character;
+
+            while ((character = fileReader.read()) != -1) {
+                fileInString.append((char) character);
+            }
+
+            System.out.println(fileInString);
+
+            boolean result = fileInString.toString().contains("Neveselbert");
+            Assertions.assertTrue(result);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file);
+        } catch (IOException e) {
+            System.out.println("Unable to read file: " + file);
         }
-
-        System.out.println(fileInString);
-
-        boolean result = fileInString.toString().contains("Neveselbert");
-        Assertions.assertTrue(result);
     }
 }
